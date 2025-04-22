b51adc79931cd0d4cb9b91418100ebfe5676ffee
Lam <buinguyenlam@gmail.com>
2025-04-22 11:10:49 +0200

    add UWBCommand: - "uwb start" to start - "uwb stop" to stop

---
+++ include/commands/UWBCommand.h
@@ -0,0 +1,54 @@
+#ifndef UWB_COMMAND_H
+#define UWB_COMMAND_H
+
+#include "interfaces/IExtendedCommand.h"
+#include "managers/UWBManager.h"
+#include "managers/ConfigManager.h"
+static TaskHandle_t xUWBCmdHandle;
+
+class UWBCommand : public IExtendedCommand
+{
+private:
+    UWBManager &uwbManager;
+    ConfigManager &configManager;
+
+    bool startUWBCmd(const std::vector<String> &args, ICommandContext &context);
+    bool stopUWBCmd(const std::vector<String> &args, ICommandContext &context);
+
+public:
+    UWBCommand()
+        : uwbManager(UWBManager::getInstance()),
+          configManager(ConfigManager::getInstance())
+    {
+        xUWBCmdHandle = nullptr;
+        subCommands["start"] = [this](const std::vector<String> &args, ICommandContext &context)
+        {
+            return startUWBCmd(args, context);
+        };
+        subCommandDescriptions["start"] = "Starts the Ultra Wideband module";
+        subCommandParameters["start"] = {
+            // Todo
+        };
+
+        subCommands["stop"] = [this](const std::vector<String> &args, ICommandContext &context)
+        {
+            return stopUWBCmd(args, context);
+        };
+        subCommandDescriptions["stop"] = "Stops the Ultra Wideband module";
+        subCommandParameters["stop"] = {
+            // Todo
+        };
+    }
+
+    const char *getName() const override
+    {
+        return "uwb";
+    }
+
+    const char *getDescription() const override
+    {
+        return "Configure and manage Ultra Wideband";
+    }
+};
+
+#endif
